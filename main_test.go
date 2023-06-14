2e98541650ab91339b5814dfab2be8f29e33a026
kingkia <1581690775@qq.com>
2023-06-14 15:01:14 +0800

    修改测试文件

--- main_test.go
+++ main_test.go
@@ -31,10 +31,16 @@ func TestCS(t *testing.T) {
 	go startServer(ch)
 	addr := <-ch
 
-	client, err := Dial("tcp", addr)
+	ip, port, err := net.SplitHostPort(addr)
+	if err != nil {
+		log.Fatal("SplitHostPort error")
+	}
+
+	conn, err := Dial(ip, port)
 	if err != nil {
 		log.Fatal("Client Dial Failed")
 	}
+	client := NewClient(conn)
 	res1 := client.Call("Sum", 2, 2)
 	res2 := client.Call("Product", 3, 3)
 	fmt.Printf("2 + 2 = %v\n", res1[0])
